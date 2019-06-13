c4bbda8fbc2bfd055e18116895f0d7e81d9f196a
John Stein <john.stein@fireeye.com>
2019-06-13 07:23:43 -0400

    wip

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pcwm
+
 Put Cloudwatch metrics with Node.js
+
+The idea here is to make it easy to send a single custom metric with
+multiple namespaces.
+
+Multiple dimensions are pretty much useless with custom metrics.
+
+https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/cloudwatch_concepts.html
+
+The whole reason to have multiple dimensions would be if you want to
+slice and dice (which AWS allow you to do with some of their built in
+metrics, but not with custom metrics).
+
+Here we are just making it easy to send multiple copies of a single
+metric each with a different namespaces. You will have to do your own
+aggregating, but at least you have the data split out.
+
---
+++ index.js
@@ -0,0 +1,119 @@
+#!/usr/bin/env node
+
+'use strict'
+const AWS = require('aws-sdk')
+if (process.env.AWS_PROFILE) { // eslint-disable-line
+  const credentials = new AWS.SharedIniFileCredentials({
+    profile: process.env.AWS_PROFILE // eslint-disable-line
+  })
+  AWS.config.credentials = credentials
+  AWS.config.logger = console
+}
+/* beautify preserve:start */
+const cloudwatch = new AWS.CloudWatch({ apiVersion: '2010-08-01' })
+/* beautify preserve:end */
+class Metrics {
+  constructor(options) {
+    // options.namespace
+    //
+    // options.dimensions
+    //
+    // {
+    //  dimensionName1: dimensionValue1,
+    //  dimensionName2: dimensionValue2,
+    //  dimensionName3: dimensionValue3,
+    // }
+    //
+    // options.metrics
+    //
+    // {
+    //  metricName1: { unit: 'Count', dimensions: [dimensionName2] }
+    //  metricName2: { unit: 'Count' }, // (all dimensions)
+    //  metricName3: { unit: 'Count', dimensions: [dimensionName1, dimensionName3] },
+    //  metricName4: { unit: 'Count', skipZero: true }, // don't add if value 0
+    // }
+    this.options = options
+    this.params = {
+      MetricData: [],
+      Namespace: options.namespace
+    }
+  }
+  addMetric(name, value, count = 1) {
+    let dims = JSON.parse(JSON.stringify(this.options.dimensions))
+    if (this.options.metrics[name].dimensions) {
+      dims = {}
+      for (const dimName of this.options.metrics[name].dimensions) {
+        dims[dimName] = this.options.dimensions[dimName]
+      }
+    }
+    for (const dimName of Object.keys(dims)) {
+      const md = {
+        MetricName: name,
+        Dimensions: [{
+          Name: dimName,
+          Value: dims[dimName]
+        }],
+        Timestamp: new Date(),
+        Unit: this.options.metrics[name].unit,
+        Counts: Array.isArray(count) ? count : [count],
+        Values: Array.isArray(value) ? value : [value]
+      }
+      if (this.options.metrics[name].skipZero) {
+        for (let i = md.Values.length - 1; i >= 0; --i) {
+          if (md.Values[i] === 0) {
+            md.Counts.splice(i, 1)
+            md.Values.splice(i, 1)
+          }
+        }
+      }
+      this.params.MetricData.push(md)
+    }
+  }
+  putMetricData() {
+    if (this.params.MetricData.length) {
+      return cloudwatch.putMetricData(this.params).promise()
+    }
+    return Promise.resolve({ Info: 'No metrics to put. Doing nothing.' })
+  }
+  toString(indent) {
+    return JSON.stringify(this, null, indent)
+  }
+}
+
+if (require.main === module) {
+  /* beautify preserve:start */
+  const options = {
+    namespace: 'SOME/namespace',
+    dimensions: {
+      LambdaFunction: 'raw-lambda',
+      LambdaFunctionCustomer: 'raw-lambda-hexint07sust01',
+    },
+    metrics: {
+      LambdaInits: { unit: 'Count', dimensions: [ 'LambdaFunction' ] },
+      BatchesSent: { unit: 'Count', skipZero: true },
+      EventsSent: { unit: 'Count' },
+    }
+  }
+  /* beautify preserve:end */
+  const m = new Metrics(options)
+  m.putMetricData()
+    .then((result) => {
+      console.log('RESULT', result)
+    })
+    .catch((err) => {
+      console.log('ERROR', err)
+    })
+
+  console.log(m.toString(3))
+  m.addMetric('BatchesSent', [3823, 0, 4], [1, 4, 2])
+  console.log(m.toString(3))
+  m.addMetric('EventsSent', 0)
+  console.log(m.toString(3))
+  m.putMetricData()
+    .then((result) => {
+      console.log('RESULT', result)
+    })
+    .catch((err) => {
+      console.log('ERROR', err)
+    })
+}
